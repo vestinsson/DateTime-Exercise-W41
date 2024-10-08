e64846ed99a4a6e4c4a972702fdb280a988585bc
vestinsson <vestinsson@outlook.com>
2024-10-08 11:08:10 +0200

    Add project files.

---
+++ Person.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateTime_Exercise_W41
+{
+    internal class Person
+    {
+        public Person(DateTime dt, int id, string name)
+        {
+            this.dt = dt;
+            this.id = id;
+            Name = name;
+        }
+
+        public string Name {  get; set; }
+        public int id { get; set; }
+        public DateTime dt { get; set; }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿
+
+using DateTime_Exercise_W41;
+
+List<Person> people = new List<Person>();
+
+while (true)
+{
+    DateTime dt;
+
+    Console.Write("Date (yyyy-MM-dd): ");
+    string temp = Console.ReadLine();
+    if (temp.ToLower() == "exit")
+    {
+        break;
+    }
+    else
+    {
+        dt = Convert.ToDateTime(temp);
+    }
+
+    Console.Write("Id: ");
+    int id = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Name: ");
+    string n = Console.ReadLine();
+
+    Person p = new Person(dt, id, n);
+    people.Add(p);
+
+}
+
+List<Person> sortedPersons = people.OrderBy(p => p.dt).ToList();
+
+Console.WriteLine("Date Id Name");
+foreach (Person p in sortedPersons)
+{
+    Console.WriteLine(p.dt + " " + p.id + " " + p.Name );
+}
+Console.ReadKey();
